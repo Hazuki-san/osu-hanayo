d26794ccfe42a721d68933b046473f406426afbf
Aoba Suzukaze <neko.aoba.suzukaze@gmail.com>
2019-10-03 15:42:04 +0700

    [BROKEN] Add some resource and edit some templates

--- templates.go
+++ templates.go
@@ -24,6 +24,7 @@ import (
 var templates = make(map[string]*template.Template)
 var baseTemplates = [...]string{
 	"templates/base.html",
+	"templates/login_box.html",
 	"templates/navbar.html",
 	"templates/simplepag.html",
 }
