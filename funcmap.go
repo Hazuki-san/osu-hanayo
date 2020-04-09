a9160f2d0522b21af98486c7d7012a220abcbe01
Hazuki Onohara <neko.aoba.suzukaze@gmail.com>
2020-04-09 13:56:07 +0700

    wait wtf lol

--- funcmap.go
+++ funcmap.go
@@ -167,16 +167,6 @@ var funcMap = template.FuncMap{
 		}
 		return template.HTML(fmt.Sprintf(`%s`, c))
 	},
-	"countryName": func(s string, name bool) template.HTML {
-		var c string
-		if name {
-			c = countryReadable(s)
-			if c == "" {
-				return ""
-			}
-		}
-		return template.HTML(fmt.Sprintf(`%s`, c))
-	},
 	"navbarCountry": func(s string, name bool) template.HTML {
 		var c string
 		if name {
